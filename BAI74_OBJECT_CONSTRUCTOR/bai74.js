c00ba7e9a8f226033e9ea5fcf64b1d1c931e6084
Linh Nguyen <vanlinh11052004nguyenvan@gmail.com>
2024-01-02 18:59:01 +0700

    upload new files

---
+++ BAI74_OBJECT_CONSTRUCTOR/bai74.js
@@ -0,0 +1,33 @@
+// Oject constructor 
+// với từ khóa function User, từ khóa this mô tả thuộc tính và phương thức  có thể có khi khởi tạo một object /
+// this ở getName là đổi tượng được tạo ra  khi khỏi tạo 
+/*
+Anh Sơn
+// Expresion function 
+function User(firstName, lastName, avatar){
+    this.firstName = firstName ;// this.firstName tu dong duoc tao ra
+    this.lastName = lastName;
+    this.avatar= avatar;
+    this.getName = function(){
+        return `${this.firstName} ${this.lastName}`
+    }
+}
+var author = new User('Linh','Nguyen Van','anh.png');
+
+var user= new User('Hoa','Le','avatar');
+user.email='vanlinh@gmail.com'// thêm thuộc tính 
+user.title='chia sẻ dạo tại F8!';
+author.comment='Liệu có phải đây là Linh Nguyễn trong truyền thuyết?'
+console.log(author);
+console.log(user.firstName)
+
+console.log(author.getName())
+
+// Decleration function 
+var home = function(width, height){
+    this.width= width;
+    this.height = height;
+}
+*/
+// Luyện tập 
+
