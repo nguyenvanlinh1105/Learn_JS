dca56d7c3780bc002ab6bb3ed36766f7979e99d2
Linh Nguyen <vanlinh11052004nguyenvan@gmail.com>
2024-01-10 00:10:34 +0700

    upload new file

---
+++ BAI78_DATE/bai78.js
@@ -0,0 +1,15 @@
+// Đối tượng Date 
+
+var date = new Date();
+console.log(date.getDate()+"/"+date.getMonth()+1+"/"+date.getFullYear())
+
+console.log(typeof date)
+console.log(date.getHours())
+console.log(date.getMinutes())
+console.log(date.getMilliseconds())
+
+console.log(typeof ngay )
+
+
+
+
---
+++ Bai80_MATHOBJECT/bai80.js
@@ -0,0 +1,26 @@
+console.log(Math.PI)
+console.log(Math.round(2.5))
+
+console.log(Math.abs(-3))
+console.log(Math.ceil(5.3)) // làm tròn trên 
+console.log(Math.floor(4.5))// làm tròn dưới 
+console.log(Math.random())// số thập phân nhỏ hơn một 
+console.log(Math.floor(Math.random()*100))// tạo số ngẫu nhiên theo đoạn 
+
+console.log(Math.min(3, 4, 1))
+console.log(Math.max(424,2,425))
+
+var random  = Math.floor(Math.random()*5);
+var bonus=[
+    '10 coin',
+    '20 coin',
+    '30 coin',
+    '40 coin',
+    '50 coin'
+]
+console.log(bonus[random])
+
+
+
+
+
