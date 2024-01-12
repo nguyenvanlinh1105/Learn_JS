63f01e121bdc23f868117f2c592a5d5fa755d5d8
Linh Nguyen <vanlinh11052004nguyenvan@gmail.com>
2024-01-12 23:56:51 +0700

    upload new file

---
+++ BAI130_DOM/BAI130.js
@@ -0,0 +1,14 @@
+// HTML DOM 
+// 1. ELEMENT
+// 2. ATTRIBUTE
+// 3. TEXT
+
+
+// HTML DOM không phải là JS , JS chỉ cung cấp những bộ phương tiên để truy cập vào HTML DOM mà thôi 
+
+//------------
+// JAVASCRIPT: Browser, server (Nodejs)
+// Browser : HTML-> DOM -> WEB API
+// (API) Application programing interface 
+
+
---
+++ BAI131_DOCUMENT_OBJ/bai131.js
@@ -0,0 +1,3 @@
+console.log(document)
+
+document.write("HELLO LINH NGUYEN ")
