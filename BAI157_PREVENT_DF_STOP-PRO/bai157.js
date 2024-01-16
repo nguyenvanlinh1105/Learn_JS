88b7f97606185189a77abe0882e0374ccc98b04b
Linh Nguyen <vanlinh11052004nguyenvan@gmail.com>
2024-01-17 00:30:11 +0700

    upload new files

--- BAI157_PREVENT_DF_STOP-PRO/bai157.js
+++ BAI157_PREVENT_DF_STOP-PRO/bai157.js
@@ -3,11 +3,20 @@
 var aElement = document.querySelectorAll('a')
 var aElements = document.anchors;// lấy ra các thẻ a có name
 var aElement1 = document.links;// trả về tất cả thẻ a
-console.log(aElement)
+
+for(var i =0;i<aElement.length;i++){
+    aElement[i].onclick = function(e){
+        if(!e.target.href.startsWith("https://f8.edu.vn")){
+            e.preventDefault();// ngăn chặn hành vi mặc định của thẻ a 
+        }
+    }
+}
 
 
-
-
+var ulElemet = document.querySelector('ul')
+ulElemet.onclick  = function(e){
+    console.log(e.target)
+}
 
 
 
