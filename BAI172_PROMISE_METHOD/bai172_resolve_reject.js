d01943163cbb038b9cb0c19f2cb298d09130566f
Linh Nguyen <vanlinh11052004nguyenvan@gmail.com>
2024-01-18 01:00:24 +0700

    upload new files

--- BAI172_PROMISE_METHOD/bai172_resolve_reject.js
+++ BAI172_PROMISE_METHOD/bai172_resolve_reject.js
@@ -1,3 +1,48 @@
 // promise.resolve
 // promise.reject 
 // promise.all
+
+var promise = new Promise(
+    function(resolve, reject){
+        resolve('Success!')
+    }
+)
+
+promise
+    .then(function(message){
+        console.log(message)
+    })
+    .catch(function(){
+        console.log(message)
+    })
+
+    var prm= Promise.resolve("Success")// xác định là luôn thành công không có thất bại
+
+    //var prms = Promise.reject("Error")// xác định là luôn thật bại 
+
+
+    // chạy song song , sử dụng kết hợp dữ liệu 2 phần là res và rej
+
+    var prm1 = new Promise(
+        function(resolve){
+            setTimeout(function(){
+                resolve([1])
+            },2000)
+        }
+    )
+    var prm2= new Promise(function(resolve){
+        setTimeout(function(){
+            resolve([2,3])
+        },5000)
+    })
+    // nếu làm nối đuôi thì mất 7s nếu làm kết hợp all thì mất 5s
+
+Promise.all([prm1, prm2])
+        .then(function(result){
+            var result1= result[0]
+            var result2= result[1]
+            console.log(result1.concat(result2))
+        })
+        // .then(function(result1, result2){
+        //     console.log(result1.concat(result2))
+        // }) hoặc là như thế này 
---
+++ BAI173_PROMISE_EX/bai173.js
@@ -0,0 +1,32 @@
+//example 
+// 
+
+var user=[
+    {
+        id:1,
+        name:'Linh Nguyễn',
+    },
+    {
+        id:2,
+        name:'Sơn Đặng'
+    },
+    {
+        id:3,
+        name:'Hòa Lê'
+    }
+]
+
+// nơi luu trưc comment
+
+var comments = [
+    {
+        id:1,
+        user_id:1,
+        content:'Anh Sơn chưa ra video à'
+    },
+    {
+        id:2,
+        user_id:2,
+        content:'Vừa ra xong e ơi'
+    }
+]
