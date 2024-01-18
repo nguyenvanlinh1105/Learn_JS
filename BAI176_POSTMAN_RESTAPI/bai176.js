bc19da48f556466c18dc9e304213c1c81bf60be5
Linh Nguyen <vanlinh11052004nguyenvan@gmail.com>
2024-01-19 00:34:14 +0700

    upload new files

---
+++ BAI176_POSTMAN_RESTAPI/bai176.js
@@ -0,0 +1,10 @@
+// JSON SERVER -> API Server(Fake)/ Mock API=>OK
+
+
+// CRUD//  Tiêu chuẩn rest 
+// CREATE : tạo mới => Post
+// Read: Lấy dữ liệu => get
+// Update : chỉnh sử  => put / patch
+// Delete : xóa => delete 
+// postman 
+
