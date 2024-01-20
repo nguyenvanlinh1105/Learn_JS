39dd7453069965d5119c37c808b4ce270179999e
Linh Nguyen <vanlinh11052004nguyenvan@gmail.com>
2024-01-20 23:33:57 +0700

    upload new files

--- BAI193_TABSUI/bai193.js
+++ BAI193_TABSUI/bai193.js
@@ -1,1 +1,4 @@
-
+const $ = document.querySelector.bind(document)
+// bind(document) được sử dụng để gắn kết hàm querySelector với đối tượng document. 
+const $$ = document.querySelectorAll.bind(document);
+// bind(document) được sử dụng để gắn kết hàm querySelectorAll với đối tượng document. 
