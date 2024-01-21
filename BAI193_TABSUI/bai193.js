9d56ff65131183ac1300280f03201c70bdc78e90
Linh Nguyen <vanlinh11052004nguyenvan@gmail.com>
2024-01-21 10:12:34 +0700

    upload new files

--- BAI193_TABSUI/bai193.js
+++ BAI193_TABSUI/bai193.js
@@ -2,3 +2,36 @@ const $ = document.querySelector.bind(document)
 // bind(document) được sử dụng để gắn kết hàm querySelector với đối tượng document. 
 const $$ = document.querySelectorAll.bind(document);
 // bind(document) được sử dụng để gắn kết hàm querySelectorAll với đối tượng document. 
+
+const tabs = $$('.tab-item');
+const panes =$$('.tab-pane')
+const tabActive = $('.tab-item.active')
+const line = $('.tabs .line')
+
+console.log(tabActive)
+tabs.forEach((tab,index) => {
+    const pane = panes[index]
+    tab.onclick= function(){
+        $('.tab-item.active').classList.remove('active')
+        $('.tab-pane.active').classList.remove('active')
+        this.classList.add('active')
+        pane.classList.add('active')
+        Object.assign(line.style,{
+            left : this.offsetLeft+'px',
+            width:this.offsetWidth+'px'
+        })
+    }
+    }
+);
+
+
+
+
+
+
+
+
+
+
+
+
