03abd74348b50a492e547fa651dda3963182dedb
Linh Nguyen <vanlinh11052004nguyenvan@gmail.com>
2024-01-25 01:28:37 +0700

    upload new files

--- BAI194_MP3/assets/js/main.js
+++ BAI194_MP3/assets/js/main.js
@@ -19,9 +19,16 @@ const audio = $('#audio')
 const playBtn = $('.btn-toggle-play')
 const player  = $('.player')
 const progress =$('#progress');
-
+const btnNext = $('.btn-next')
+const btnPrev = $('.btn-prev')
+const btnRandom = $('.btn-random')
+const btnRepeat = $('.btn-repeat')
+const playlist = $('.playlist')
 const app={
     currentIndex :0,
+    isPlaying: false,
+    isRandom:false,
+    isRepeat :false,
     songs: [
         {
             name:'Trái đất ôm mặt trời',
@@ -53,7 +60,6 @@ const app={
             path:'./assets/mp3/song5.mp3',
             image:'./assets/img/song5.png'
         },
-        ,
         {
             name:'Yêu em rất nhiều',
             singer:'Hoàng Tôn',
@@ -75,9 +81,9 @@ const app={
     ],
     render:function(){
 
-        const htmls = this.songs.map(song=>{
+        const htmls = this.songs.map((song,index)=>{
             return`
-            <div class="song">
+            <div class="song ${index===this.currentIndex?'active':'' }">
                 <div class="thumb" style="background-image: url('${song.image}')">
                 </div>
                 <div class="body">
@@ -90,7 +96,7 @@ const app={
             </div>
             `
         })
-        $('.playlist').innerHTML = htmls.join('\n')
+        playlist.innerHTML = htmls.join('\n')
     },
     defineProperties:function(){
         // định nghĩa thuộc tính 
@@ -103,6 +109,15 @@ const app={
     handleEvents:function(){
         const _this = this;
         const cdWidth = cd.offsetWidth;
+        // xử lí cd quay va dừng 
+        const cdThumbAnimate= cdThumb.animate([
+            {transform:'rotate(360deg'},
+        ],{
+            duration:10000,
+            interations:Infinity
+        })// trả về một đối tượng. 
+
+        cdThumbAnimate.pause()
         // xử lí phóng to thu nhỏ cd 
         document.onscroll= function(){
             // console.log(document.documentElement.scrollTop) // cách 1 
@@ -116,14 +131,24 @@ const app={
         }
         // xử lí khi click play:
         playBtn.onclick = function () {
-            if(audio.paused){
-                audio.play();
-                player.classList.add('playing');
-            }else{
+            if(_this.isPlaying){
                 audio.pause();
-                player.classList.remove('playing');
+            }else{
+                audio.play();
             }
         };
+        // khi song được play:
+        audio.onplay=function(){
+            _this.isPlaying=true;
+            player.classList.add('playing');
+            cdThumbAnimate.play();
+        }
+        // khi song được pause 
+        audio.onpause= function(){
+            _this.isPlaying= false;
+            player.classList.remove('playing');
+            cdThumbAnimate.pause()
+        }
         // khi tiến độ bài hát thay đổi 
         audio.ontimeupdate= function(){
             if(audio.duration){
@@ -132,23 +157,108 @@ const app={
             }
             updateProgressColor();
         }
+        // khi tua bài hát 
         progress.onchange=function(e){
             const timeAudio = e.target.value/100*audio.duration// timeAudio là % mình chia 100 lấy được số phần trăm * tổng thời gian bài hát.
             audio.currentTime=timeAudio
             updateProgressColor();
         }
         // xử lí cho thanh progress có màu chạy theo 
-        function updateProgressColor() {
-            const percent = (progress.value - progress.min) / (progress.max - progress.min);
-            const color = `linear-gradient(to right, var(--primary-color) ${percent * 100}%, #ddd ${percent * 100}%)`;
-            progress.style.background = color;
+        // function updateProgressColor() {
+        //     const percent = (progress.value - progress.min) / (progress.max - progress.min);
+        //     const color = `linear-gradient(to right, var(--primary-color) ${percent * 100}%, #ddd ${percent * 100}%)`;
+        //     progress.style.background = color;
+        // }
+        function updateProgressColor(){
+            const percent = (progress.value-progress.min)/(progress.max-progress.min);
+            const color =`linear-gradient(to right, var(--primary-color) ${percent*100}%, #ddd ${percent*100}%)`;
+            progress.style.background=color;
         }
+        // next xong 
+        btnNext.onclick = function(){
+            if(_this.isRandom){
+                _this.playRandomSong();
+            }else{
+                _this.nextSong();
+            }
+            audio.play();
+            _this.render();
+            _this.scrollToActiveSong();
+
+        }
+        // prev song 
+        btnPrev.onclick = function(){
+            if(_this.isRandom){
+                _this.playRandomSong();
+            }else{
+                _this.prevSong();
+            }
+            audio.play();
+            _this.render();
+            _this.scrollToActiveSong();
+        }
+        // random song 
+        btnRandom.onclick = function(){
+            _this.isRandom= !_this.isRandom
+            btnRandom.classList.toggle('active', _this.isRandom)
+        }
+        // audio ended =>next song 
+        audio.onended= function(){
+            if(_this.isRepeat){
+                audio.play();
+            }else{
+                btnNext.click();
+            }
+            
+        }
+        // audio repeat 
+        btnRepeat.onclick = function(){
+            _this.isRepeat= !_this.isRepeat
+            btnRepeat.classList.toggle('active',_this.isRepeat)
+        }
+        // xử lí khi click vào bài thì play
+        playlist.onclick = function(e){
+            console.log(e.target)
+        } 
+        
     },
     loadCurrentSong:function(){
-        heading.textContent= this.currentSong.name
-        cdThumb.style.backgroundImage =`url('${this.currentSong.image}')`
-        audio.src=this.currentSong.path 
+        heading.textContent = this.currentSong.name;
+        cdThumb.style.backgroundImage = `url('${this.currentSong.image}')`;
+        audio.src = this.currentSong.path;
+  
     },
+    nextSong:function(){
+        this.currentIndex++;
+        if(this.currentIndex>=this.songs.length){
+            this.currentIndex=0
+        }
+        this.loadCurrentSong();
+    },
+    prevSong:function(){
+        this.currentIndex--;
+        if(this.currentIndex<0){
+            this.currentIndex=this.songs.length-1;
+        }
+        this.loadCurrentSong();
+    },
+    playRandomSong:function(){
+        let newIndex;
+        do{
+           newIndex = Math.floor(Math.random()*this.songs.length)
+        }while(this.currentIndex===newIndex)
+        this.currentIndex=newIndex;
+        this.loadCurrentSong();
+    },
+    scrollToActiveSong:function(){
+       setTimeout(function(){
+            $('.song.active').scrollIntoView({
+                behavior:'smooth',
+                block :'nearest'
+            },300);
+       })// index 1, 2, 3, thì cho nó kéo xuông thêm nữa. 
+    },
+
 
     start :function(){
         this.defineProperties();// định nghĩa các thuộc tính cho object
